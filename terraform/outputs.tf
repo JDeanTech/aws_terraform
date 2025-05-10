acecb64f4e0994ed684df1ab7cfd8c2c136db1e8
Joshua <joshuadean0424@gmail.com>
2025-05-09 22:40:34 -0500

    edited outputs file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,14 @@ output "iam_user_name" {
   value = "aws.iam.new_user.name"
 }
 
+output "iam_user_policy" {
+  value = "aws.iam.new_user.policy"
+}
+
+output "instance_name" {
+  value = aws_instance.test.tags["Name"]
+}
+
 output "instance_public_ip" {
   value = aws_instance.test.public_ip
 }
